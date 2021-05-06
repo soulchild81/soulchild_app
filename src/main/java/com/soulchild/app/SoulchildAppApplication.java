0def1a0caad28c4da4017b56fc97dd1b7e9ede7b
soulchild81 <soulchild81@naver.com>
2021-05-06 11:15:04 +0900

    initial commit

---
+++ src/main/java/com/soulchild/app/SoulchildAppApplication.java
@@ -0,0 +1,13 @@
+package com.soulchild.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulchildAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoulchildAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soulchild/app/SoulchildAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulchild.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulchildAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
