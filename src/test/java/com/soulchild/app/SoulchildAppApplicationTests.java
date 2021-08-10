6b37a150d6b116a0d1c526c5bc1280c1802ef82c
soulchild81 <soulchild81@naver.com>
2021-08-10 18:09:21 +0900

    fix

--- src/test/java/com/soulchild/app/SoulchildAppApplicationTests.java
+++ src/test/java/com/soulchild/app/SoulchildAppApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.util.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class SoulchildAppApplicationTests {
+public class SoulchildAppApplicationTests {
 
     @Test
     public void contextLoads() {
