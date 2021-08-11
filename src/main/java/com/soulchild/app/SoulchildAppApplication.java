97a359b8bf7caee8e3df5c715537435160769bbd
soulchild81 <soulchild81@naver.com>
2021-08-11 18:36:18 +0900

    fix

--- src/main/java/com/soulchild/app/SoulchildAppApplication.java
+++ src/main/java/com/soulchild/app/SoulchildAppApplication.java
@@ -2,10 +2,11 @@ package com.soulchild.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SoulchildAppApplication {
 
     public static void main(String[] args) {
