c4334c011a4b078a92be0e124f74ec14e3dc3aa4
soulchild81 <soulchild81@naver.com>
2021-08-11 18:17:37 +0900

    fix

--- src/main/java/com/soulchild/app/web/api/v1/UserController.java
+++ src/main/java/com/soulchild/app/web/api/v1/UserController.java
@@ -1,8 +1,8 @@
 package com.soulchild.app.web.api.v1;
 
-import com.soulchild.app.domain.User;
-import com.soulchild.app.dto.UserResponseDto;
-import com.soulchild.app.dto.UserSaveRequestDto;
+//import com.soulchild.app.domain.User;
+//import com.soulchild.app.dto.UserResponseDto;
+//import com.soulchild.app.dto.UserSaveRequestDto;
 import com.soulchild.app.service.UserService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -17,19 +17,19 @@ public class UserController {
 
     private final UserService userService;
 
-    @PutMapping("user/add")
-    public Long save(@RequestBody UserSaveRequestDto requestDto){
-        return userService.save(requestDto);
-    }
-
-    @PutMapping("user/{user_id}")
-    public Long update(@PathVariable Long user_id , @RequestBody UserSaveRequestDto requestDto){
-        userService.update(user_id , requestDto);
-        return null;
-    }
-
-    @GetMapping("user/{user_id}")
-    public UserResponseDto getUser(@PathVariable Long user_id){
-        return userService.findById(user_id);
-    }
+//    @PutMapping("user/add")
+//    public Long save(@RequestBody UserSaveRequestDto requestDto){
+//        return userService.save(requestDto);
+//    }
+//
+//    @PutMapping("user/{user_id}")
+//    public Long update(@PathVariable Long user_id , @RequestBody UserSaveRequestDto requestDto){
+//        userService.update(user_id , requestDto);
+//        return null;
+//    }
+//
+//    @GetMapping("user/{user_id}")
+//    public UserResponseDto getUser(@PathVariable Long user_id){
+//        return userService.findById(user_id);
+//    }
 }
